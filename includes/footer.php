df81d314c45f9f693f2c183caaee77602dda7af3
justjaaayy <justjaaayy@gmail.com>
2015-06-04 17:21:53 +0100

    Includes folder

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+</div>
+<div class="push"></div>
+<footer class="footer">
+	<div class="social">
+	<a href="https://twitter.com/" target="blank">
+		<i class="fa fa-twitter"></i>
+	</a>
+	<a href="https://dribbble.com/" target="blank">
+		<i class="fa fa-dribbble"></i>
+	</a>
+	<a href="https://github.com/" target="blank">
+	<i class="fa fa-github"></i>
+	</a>
+	</div>
+	<p>&copy; Copyright 2015 | Designed &amp; Built by Jordan</p>
+</footer>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,53 @@
+﻿<!DOCTYPE html>
+<html lang="en_GB"
+<head>
+<meta charset="utf-8">
+<meta content="IE=edge,chrome=1" http-equiv=X-UA-Compatible>
+<link rel="shortcut icon" href="favicon.png">
+<meta name="description" content="My name is Jordan. I'm a freelance graphics designer but I have a love for all things web related.">
+<meta name=viewport content="width=device-width, initial-scale=1, minimal-ui">
+<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
+<script src="js/modernizr.custom.js"></script>
+<title>Jordan • Designer &amp; Developer</title>
+</head>
+<body>
+<div class="wrapper">
+ <nav>       
+ 	<div class="logo">
+ 		<img src="images/mylogo.png" alt="">
+ 		</div>
+    <a href="#" class="nav-toggle-btn">
+    	<span class="lnr lnr-menu"></span>
+    </a>
+            
+        <ul>
+            <li><a href="index">Home</a></li>
+            <li><a href="about">About</a></li>
+            <li><a href="work">Work</a></li>
+            <li><a href="services">Services</a></li>
+            <li><a href="contact">Contact</a></li>
+        </ul>
+            
+</nav>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript">
+        
+        (function() {
+            
+            var bodyEl = $('body'),
+                navToggleBtn = bodyEl.find('.nav-toggle-btn');
+            
+            navToggleBtn.on('click', function(e) {
+                bodyEl.toggleClass('active-nav');
+                e.preventDefault();
+            });
+            
+            
+            
+        })();
+        
+        
+</script>
+
