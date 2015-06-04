89235b1123b02fa4b7deb2a3742abec443777e6d
justjaaayy <justjaaayy@gmail.com>
2015-06-04 17:03:00 +0100

    Index add

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include("includes/header.php"); ?>
+<div class="content">
+<div class="brand">
+	<img src="images/mylogo.png" alt="">
+</div>
+<div class="jumbo">
+<h2>Hi, I'm Jordan &amp;
+<br>
+ I'd like to work with <a href="contact.php">you.</h2></a>
+ <?php include("includes/footer.php"); ?>
+</div>
+</div>
+
+
