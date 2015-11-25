09f124d4e097576a0100b3ec4f67777fbf00bb23
justjaaayy <justjaaayy@gmail.com>
2015-11-25 18:39:45 +0000

    updated index.php

--- index.php
+++ index.php
@@ -1,14 +1,58 @@
-<?php include("includes/header.php"); ?>
-<div class="content">
-<div class="brand">
-	<img src="images/mylogo.png" alt="">
-</div>
-<div class="jumbo">
-<h2>Hi, I'm Jordan &amp;
-<br>
- I'd like to work with <a href="contact.php">you.</h2></a>
- <?php include("includes/footer.php"); ?>
-</div>
+<?php
+$page = 'index';
+include('includes/header.php');
+?>
+
+<div class="row center-xs">
+    <div class="col-xs-6">
+        <div class="jumbo">
+          <h1>Hi, I'm Jordan.</h1>
+          <h3>A designer &amp; developer from the UK.</h3>
+        </div>
+    </div>
 </div>
 
+<div class="row center-xs index-menu">
+    <div class="col-xs-2 first-xs">
+        <div class="box menu-item">
+            <a class="icon-user" href="#" title="About me"></a>
+            <div class="menu-text">
+                <p class="description-title">About</p>
+                <hr>
+                <p>Find out who I am and what I do.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-2">
+        <div class="box menu-item">
+              <a class="icon-monitor" href="#" title="See my work"></a>
+              <div class="menu-text">
+                  <p class="description-title">Work</p>
+                  <hr>
+                  <p>Take a look at what I’ve done and see if it’s what you’re looking for.</p>
+              </div>
+        </div>
+    </div>
+    <div class="col-xs-2">
+        <div class="box menu-item">
+            <a class="icon-newspaper" href="#" title="Read my blog"></a>
+            <div class="menu-text">
+                <p class="description-title">Blog</p>
+                <hr>
+                <p>Read my articles and see what I’m currently up to.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-2">
+        <div class="box menu-item">
+              <a class="icon-mail" href="#" title="Get in touch with me"></a>
+              <div class="menu-text">
+                  <p class="description-title">Contact</p>
+                  <hr>
+                  <p>Get in touch and hire me to help you bring your idea to life.</p>
+              </div>
+        </div>
+    </div>
+</div>
 
+<?php include("includes/footer.php"); ?>
